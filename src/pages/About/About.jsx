da335ab35905ca4d4c10c1855e66809ba6875887
Saeb Naser <56490771+Saeb0x@users.noreply.github.com>
2023-01-09 00:52:01 +0800

    About Deska
    Added new details to the 'Abous Us' page, for more information on Deska

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -16,6 +16,7 @@ const About = () => {
           </div>
           <div className='about-text'>
             <h2 className='about-title fs-26 ls-1'>About Deska</h2>
+            <!--About Deska-->
             <p className='fs-17'>Deciding what book to read is always a challenge for some of us. Some people would rather turn to a friend than any random person or bestseller list, so imagine having a place where you could see your friends' or classmates' bookshelves and see what they think of all the books they read. <b>Deska</b> is that place,  It's a place where you can see the books your friends are reading. In addition to that, you'll be able to organize what you've read (or want to read) and you can even write reviews to tell people what you think. Start now by finding your next favorite book and join this journey with your friends to explore new territories, gather information, and expand your mind.</p>
             <p className='fs-17'>Deska - A Product of Tangent Tech</p>
           </div>
